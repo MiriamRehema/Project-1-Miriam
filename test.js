2862c6ee7b7f33d3b25a56d9d731ea1ee58ea498
MiriamRehema <rehemamiriam30@gmail.com>
2023-07-11 01:33:08 +0300

    index.html

---
+++ test.js
@@ -0,0 +1,11 @@
+//const registeredData = {
+ //
+// Function to populate the details in an HTML element
+fetch(' http://localhost:3000/Registered',function(data){
+console.log(data)
+})
+
+
+
+
+//
